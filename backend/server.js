122110982ae2365741a5c3457b12ebd4bf056b63
DebojitDas2003 <debojitdas0110@gmail.com>
2023-08-26 14:04:53 +0530

    Initial update

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+// Middleware setup (body parsing, CORS, etc.)
+app.use(express.json());
+// Add more middleware if needed
+
+// Define routes
+// Example:
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+// Start the server
+const PORT = process.env.PORT || 6969;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
