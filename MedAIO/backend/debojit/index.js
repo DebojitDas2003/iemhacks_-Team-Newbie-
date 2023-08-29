f19ccee1c2679aaf29756caa17141c0e932a7461
DebojitDas2003 <debojitdas0110@gmail.com>
2023-08-29 12:59:10 +0530

    minor updates

--- MedAIO/backend/debojit/index.js
+++ MedAIO/backend/debojit/index.js
@@ -12,7 +12,16 @@ app.use(
   })
 );
 
-mongoose.connect('mongodb+srv://Debojit:raja1234@medaio.inpray3.mongodb.net/doctor',{
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  password: String,
+});
+
+const User = mongoose.model('User',userSchema);
+
+mongoose.connect('mongodb+srv://Debojit:raja1234@medaio.inpray3.mongodb.net/',{
 useNewUrlParser: true,
 useUnifiedTopology:true
 });
@@ -22,7 +31,7 @@ var db = mongoose.connection;
 db.on("error", () => console.log("Error Connecting to database"));
 db.once("open", () => console.log("Connected to Databse"));
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async(req, res) => {
   var name = req.body.name;
   var email = req.body.email;
   var phone = req.body.phone;
@@ -35,13 +44,14 @@ app.post("/signup", (req, res) => {
     password: password,
   };
 
-  db.collection("users").insertOne(data, (err, collection) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Login to enter");
+  try {
+    await User.create(data);
+    console.log("Signup successful. Login to enter");
     return res.redirect("login.html");
-  });
+  } catch (err) {
+    console.error("Error during signup:", err);
+    res.status(500).send("Error during signup");
+  }
 });
 
 
